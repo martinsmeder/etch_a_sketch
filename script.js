3394ed3e67ce613d477d8ac54b63acdc5807aba5
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-01-21 18:18:37 +0100

    Added max square amount and auto clear of grid

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-// create divs
-for (let i = 0; i < 100; i++) {
-    const flexContainer = document.createElement("div");
-    flexContainer.classList.add('flexContainer');
-    divContainer.appendChild(flexContainer);   
+const container = document.querySelector('#container');
+const flexContainer = document.querySelector('.flexContainer');
+const flexItem = document.querySelector('.flexItem');
+const squareButton = document.querySelector('#create');
 
-    for (let i = 0; i < 100; i++) {
-        const flexItem = document.createElement("div");
-        flexItem.classList.add('flexItem');
-        flexContainer.appendChild(flexItem);
-    }  
-}
+squareButton.addEventListener('click', createGrid)
+
+
+function createGrid() {
+    // clear grid
+    container.replaceChildren();
+
+    let squareAmount = prompt("Squares per side? (max 100)");
+
+    if (squareAmount <= 100) {
+        // create divs
+        for (let i = 0; i < squareAmount; i++) {
+        const flexContainer = document.createElement("div");
+        flexContainer.classList.add('flexContainer');
+        container.appendChild(flexContainer); 
+
+            for (let i = 0; i < squareAmount; i++) {
+                const flexItem = document.createElement("div");
+                flexItem.classList.add('flexItem');
+                flexContainer.appendChild(flexItem);
+            }  
+        } 
+    } else {
+        alert("Too many squares! Max 100 per side.");
+        createGrid();
+    }
+};
+
+
+
+
+
+
