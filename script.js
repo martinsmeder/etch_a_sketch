3d3d3fcfeae73319dfbe342b5386c27b52c567ad
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-01-25 20:59:12 +0100

    Added buttons to change color

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 const container = document.querySelector('#container');
-const flexContainer = document.querySelector('.flexContainer');
-const flexItem = document.querySelector('.flexItem');
 const squareButton = document.querySelector('#create');
+const changeColor = document.querySelector('#color')
+const blackColor = document.querySelector('#black')
+
 
 squareButton.addEventListener('click', createGrid)
 
+changeColor.addEventListener('click', () => {
+    document.querySelectorAll('div > div > div ').forEach(item => {
+        item.addEventListener('mouseover', () => {
+            const newBgColor = randomColor();
+            item.style.setProperty('background-color', newBgColor);
+        })
+    })
+})  
+
+blackColor.addEventListener('click', () => {
+    document.querySelectorAll('div > div > div ').forEach(item => {
+        item.addEventListener('mouseover', () => {
+            item.style.background = 'black';
+        })
+    })
+})  
+
+// initial grid
+for (let i = 0; i < 16; i++) {
+    const flexContainer = document.createElement("div");
+    flexContainer.classList.add('flexContainer');
+    container.appendChild(flexContainer); 
+
+        for (let i = 0; i < 16; i++) {
+            const flexItem = document.createElement("div");
+            flexItem.classList.add('flexItem');
+            flexContainer.appendChild(flexItem);
+        } 
+    } 
 
 function createGrid() {
     // clear grid
@@ -31,8 +61,13 @@ function createGrid() {
     }
 };
 
+function random(min,max) {
+    const num = Math.floor(Math.random()*(max-min)) + min;
+    return num;
+}
+  
+function randomColor() {
+    return `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
+}
 
-
-
-
-
+// https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleDeclaration/setProperty
