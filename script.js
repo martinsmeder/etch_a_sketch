1270cc4cb5f108b58584363804dcd74b6b853848
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-01-29 19:38:13 +0100

    Reworked loop and eventlisteners, added buttons

--- script.js
+++ script.js
@@ -1,66 +1,69 @@
+let currentColor = '#333333'
+let currentMode = 'color'
+
 const container = document.querySelector('#container');
-const squareButton = document.querySelector('#create');
-const changeColor = document.querySelector('#color')
-const blackColor = document.querySelector('#black')
+const colorPicker = document.querySelector('#picker')
+const colorBtn = document.querySelector('#color');
+const rainbowBtn = document.querySelector('#rainbow');
+const blackBtn = document.querySelector('#black');
+const eraserBtn = document.querySelector('#eraser');
+const clearBtn = document.querySelector('#clear');
+const gridSlider = document.querySelector('#slider');
+let sliderOutput = document.querySelector('#output');
+sliderOutput.textContent = `${gridSlider.value} x ${gridSlider.value}`;
 
+colorBtn.addEventListener('click', () => {
+  setCurrentMode('color')
+});
+rainbowBtn.addEventListener('click', () => {
+  setCurrentMode('rainbow')
+});
+blackBtn.addEventListener('click', () => {
+  setCurrentMode('black')
+});
+eraserBtn.addEventListener('click', () => {
+  setCurrentMode('eraser')
+});
+colorPicker.addEventListener('change', (e) => {
+  setCurrentColor(e.target.value)
+});
+clearBtn.addEventListener('click', clearGrid)
+gridSlider.addEventListener('mouseup', changeGridSize)
 
-squareButton.addEventListener('click', createGrid)
-
-changeColor.addEventListener('click', () => {
-    document.querySelectorAll('div > div > div ').forEach(item => {
-        item.addEventListener('mouseover', () => {
-            const newBgColor = randomColor();
-            item.style.setProperty('background-color', newBgColor);
-        })
-    })
-})  
-
-blackColor.addEventListener('click', () => {
-    document.querySelectorAll('div > div > div ').forEach(item => {
-        item.addEventListener('mouseover', () => {
-            item.style.background = 'black';
-        })
-    })
-})  
-
-// initial grid
-for (let i = 0; i < 16; i++) {
-    const flexContainer = document.createElement("div");
-    flexContainer.classList.add('flexContainer');
-    container.appendChild(flexContainer); 
-
-        for (let i = 0; i < 16; i++) {
-            const flexItem = document.createElement("div");
-            flexItem.classList.add('flexItem');
-            flexContainer.appendChild(flexItem);
-        } 
-    } 
-
-function createGrid() {
+function createGrid(size) {
     // clear grid
     container.replaceChildren();
-
-    let squareAmount = prompt("Squares per side? (max 100)");
-
-    if (squareAmount <= 100) {
-        // create divs
-        for (let i = 0; i < squareAmount; i++) {
-        const flexContainer = document.createElement("div");
-        flexContainer.classList.add('flexContainer');
-        container.appendChild(flexContainer); 
-
-            for (let i = 0; i < squareAmount; i++) {
-                const flexItem = document.createElement("div");
-                flexItem.classList.add('flexItem');
-                flexContainer.appendChild(flexItem);
-            }  
-        } 
-    } else {
-        alert("Too many squares! Max 100 per side.");
-        createGrid();
-    }
+    
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    for (let i = 0; i < size * size; i++) {
+        const gridItem = document.createElement("div");
+        gridItem.classList.add('gridItem');
+        gridItem.addEventListener('mouseover', changeColor)
+        gridItem.addEventListener('mousedown', changeColor)
+        container.appendChild(gridItem);
+    }  
 };
 
+function changeGridSize() {
+    sliderOutput.textContent = `${gridSlider.value} x ${gridSlider.value}`;
+    createGrid(gridSlider.value)
+}
+
+function clearGrid() {
+  createGrid(gridSlider.value)
+}
+
+function setCurrentMode(newMode) {
+  currentMode = newMode
+}
+
+function setCurrentColor(newColor) {
+  currentColor = newColor
+}
+
+// generate random colors
 function random(min,max) {
     const num = Math.floor(Math.random()*(max-min)) + min;
     return num;
@@ -70,4 +73,27 @@ function randomColor() {
     return `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
 }
 
-// https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleDeclaration/setProperty
+// draw on mousedown instead of hover
+let mouseDown = false
+
+document.body.addEventListener('mousedown', () => {
+  mouseDown = true;
+});
+document.body.addEventListener('mouseup', () => {
+  mouseDown = false;
+});
+
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    if (currentMode === 'rainbow') {
+        e.target.style.backgroundColor = randomColor();
+      } else if (currentMode === 'color') {
+        e.target.style.backgroundColor = currentColor
+      } else if (currentMode === 'black') {
+        e.target.style.backgroundColor = '#000'
+      } else if (currentMode === 'eraser') {
+        e.target.style.backgroundColor = '#FFF'
+      } 
+}
+
+createGrid(16);
